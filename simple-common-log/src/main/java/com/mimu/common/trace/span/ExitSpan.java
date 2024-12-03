0f254ed2060779f52a5d19c091a213ca4b176778
luyahui <primaryscholar@126.com>
2024-12-03 18:36:12 +0800

    feat: 完善 common-log 工程 填充trace相关逻辑

--- simple-common-log/src/main/java/com/mimu/common/trace/span/ExitSpan.java
+++ simple-common-log/src/main/java/com/mimu/common/trace/span/ExitSpan.java
@@ -1,7 +1,9 @@
 package com.mimu.common.trace.span;
 
+import com.mimu.common.trace.Tracer;
+
 public class ExitSpan extends TraceSpan {
-    public ExitSpan(Integer parentId, String spanName, Integer spanId) {
-        super(parentId, spanName, spanId);
+    public ExitSpan(Integer parentId, String spanName, Integer spanId, Tracer tracer) {
+        super(parentId, spanName, spanId, tracer);
     }
 }
